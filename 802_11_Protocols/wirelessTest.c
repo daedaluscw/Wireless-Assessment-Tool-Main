a013a0c726b1d5d3a915dfeb22db5a21434dddc4
Christopher Wells <daedaluscw@gmail.com>
2020-10-25 21:03:50 -0400

    Initial Test of Libraries
    The libraries are available for the the codding of the test to ensure there is WiFi capabilities.

---
+++ 802_11_Protocols/wirelessTest.c
@@ -0,0 +1,47 @@
+//
+//Programmer: Christopher Wells
+//Date 10/16/2020
+//
+//The purpose of this file is to talk to the wifi card. At this point this is a test. Once the wifi dev is passed,
+//a list of the avalible nodes can be compiled.
+//
+
+
+#include <net/if.h>
+#include <string.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <fcntl.h> //fntnl (set descriptor options)
+#include <errno.h> //errno
+#include <unistd.h>
+#include <ifaddrs.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/wireless.h>
+
+
+
+
+int main(int argc, char const *argv[])
+{
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
